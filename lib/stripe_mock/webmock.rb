9007281203d29575ff5097ec19c917b08811adeb
Grant Klinsing <grant@digitalopera.com>
2015-07-15 12:49:03 -0600

    only load webmock when needed and allow other calls

--- lib/stripe_mock/webmock.rb
+++ lib/stripe_mock/webmock.rb
@@ -1,7 +1,8 @@
-require 'webmock'
-
 module StripeMock
   def self.capture_requests
+    require 'webmock'
+
+    WebMock.allow_net_connect!
     WebMock::API.stub_request(:any, /https:\/\/api\.stripe\.com/).to_rack(MockedStripe)
   end
 
